254b3e6010db45cc42edaa01ca99cdc56ea1ca5a
shreevardhini2812 <shreevardhini28122000@gmail.com>
2025-10-05 12:21:17 +0530

    changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://project-library-1.onrender.com/api",
+  baseURL: "https://project-library-2.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
